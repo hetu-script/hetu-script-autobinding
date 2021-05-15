2091c8e7a418e991720694d291a96b94e115ed7e
rockingdice <rockingdice@gmail.com>
2021-05-15 17:29:07 +0800

    v1.0.13

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:path/path.dart' as path;
 import 'binding-generator.dart';
 import 'defines.dart';
 
-var version = '1.0.13';
+var version = '1.0.12';
 
 var files = <FileSystemEntity>[];
 
